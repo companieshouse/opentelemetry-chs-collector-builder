a68db252e224140c466b4e1ccaffc685daee3fd6
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2025-09-05 11:17:03 +0100

    CC-2567 Overriding the healthcheck_command specific to container. Loadbalancer still uses /v1/traces

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -5,13 +5,19 @@ locals {
   global_prefix               = "global-${var.environment}"
   service_name                = "opentelemetry-chs-collector" #FIXME: opentelemetry-collector
   container_port              = "4318"
-  healthcheck_port            = "13133"
+
   docker_repo                 = "opentelemetry-chs-collector" #FIXME: opentelemetry-collector
   kms_alias                   = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority   = 220
   lb_listener_paths           = [ "/v1/traces", "/v1/metrics", "/v1/logs" ]
-  healthcheck_path            = "/health" #"/health" #"/v1/traces" #FIXME
-  healthcheck_matcher         = "200" #"200" #"405" #FIXME
+  healthcheck_path            = "/v1/traces" #This is specific for the Loadbalancer
+  healthcheck_matcher         = "405" #This is specific for the Loadbalancer
+
+  # OTEL Custom collector container health check endpoint
+  container_healthcheck_port    = "13133"
+  container_healthcheck_path    = "/health"
+  container_healthcheck_matcher = "200"
+
   vpc_name                    = local.stack_secrets["vpc_name"]
   application_subnet_ids      = data.aws_subnets.application.ids
 
